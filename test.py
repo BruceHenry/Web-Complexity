a47625129fc8bb44f8b0b8e4a4a52d4dc9b423c2
arsene2803 <kartikshet@gmail.com>
2016-10-20 15:01:23 -0400

    initial parsing

--- test.py
+++ test.py
@@ -1,44 +1,65 @@
 import json
-from haralyzer import HarParser
 
-with open('cnn_test.har', 'r') as f:
-    har_parser = HarParser(json.loads(f.read()))
+import re
+
+from pprint import pprint
+
+with open('google.har', 'rb') as f:
+    data = json.loads(f.read().decode("utf-8-sig"))
 
 requestCount = 0
 cssCount = 0
 imageCount = 0
 flashCount = 0
 jsCount = 0
+xmlCount = 0
+htmlCount = 0
+jsonCount = 0
+videoCount = 0
 
-for page in har_parser.pages:
-    print(page)
-    for entry in page.entries:
+pattern_image = re.compile('image.*')
+pattern_css = re.compile("text/css.*")
+pattern_javascript = re.compile('text/.*javascript|application/.*script')
+pattern_xml = re.compile('test/xml.*|application/xml.*')
+pattern_html = re.compile('text/html.*')
+pattern_json = re.compile('application/json.*|text/.*json')
+pattern_video = re.compile('video.*')
+pattern_response = re.compile("2.*")
 
-        if har_parser.match_headers(entry, 'request', 'Host', '.*', True):
-            #if har_parser.match_status_code(entry, '2.*'):
-                requestCount = requestCount+1
+requestCount = len(data['log']['entries'])
 
-        if har_parser.match_headers(entry, 'response', 'Content-Type', 'text/css', True):
-            if har_parser.match_status_code(entry, '2.*'):
-                cssCount = cssCount+1
+for entry in data['log']['entries']:
+    if (pattern_image.match(entry['response']['content']['mimeType']) and pattern_response.match(
+            str(entry['response']['status']))):
+        imageCount += 1
+    if (pattern_css.match(entry['response']['content']['mimeType']) and pattern_response.match(
+            str(entry['response']['status']))):
+        cssCount += 1
+    if (entry['response']['content']['mimeType'] == 'application/x-shockwave-flash' and pattern_response.match(
+            str(entry['response']['status']))):
+        flashCount += 1
+    if (pattern_javascript.match(entry['response']['content']['mimeType']) and pattern_response.match(
+            str(entry['response']['status']))):
+        jsCount += 1
+    if (pattern_xml.match(entry['response']['content']['mimeType']) and pattern_response.match(
+            str(entry['response']['status']))):
+        xmlCount += 1
+    if (pattern_html.match(entry['response']['content']['mimeType']) and pattern_response.match(
+            str(entry['response']['status']))):
+        htmlCount += 1
+    if (pattern_json.match(entry['response']['content']['mimeType']) and pattern_response.match(
+            str(entry['response']['status']))):
+        jsonCount += 1
+    if (pattern_video.match(entry['response']['content']['mimeType']) and pattern_response.match(
+            str(entry['response']['status']))):
+        videoCount += 1
 
-        if har_parser.match_headers(entry, 'response', 'Content-Type', 'image.*', True):
-            if har_parser.match_status_code(entry, '2.*'):
-                imageCount = imageCount+1
-
-        if har_parser.match_headers(entry, 'response', 'Content-Type', 'application/x-shockwave-flash', True):
-            if har_parser.match_status_code(entry, '2.*'):
-                flashCount = flashCount+1
-
-        if har_parser.match_headers(entry, 'response', 'Content-Type', 'text/javascript.*', True):
-            if har_parser.match_status_code(entry, '2.*'):
-                jsCount = jsCount+1
-
-print("request number:   ", requestCount)
-print("css number:       ", cssCount)
-print("image number:     ", imageCount)
-print("flash number:     ", flashCount)
-print("javascript number:", jsCount)
-
-# if har_parser.match_request_type(entry, 'GET'):
-#     print('This is a GET request')
+print('requestCount:', requestCount)
+print('Image Count:', imageCount)
+print('cssCount:', cssCount)
+print('flashCount:', flashCount)
+print('jsCount:', jsCount)
+print('xmlCount:', xmlCount)
+print('htmlCount:', htmlCount)
+print('jsonCount:', jsonCount)
+print('videoCount:', videoCount)
